6a938f3f71528d451eaaf4afe6e6cbb4924afc55
SyanLin <475694569@qq.com>
2022-10-01 13:47:53 +0800

    🚧 First commit.

---
+++ thread_pool.h
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <future>
+#include <functional>
+
+namespace thread_pool {
+
+/**************************
+* @author   Yuan.
+* @date     2022/9/30
+* @brief    线程安全的队列
+***************************/
+template <typename Func>
+class TaskQueue {
+public:
+    TaskQueue() = default;
+    TaskQueue(const TaskQueue&) = delete;
+    TaskQueue(TaskQueue&&) = delete;
+    ~TaskQueue() = default;
+    TaskQueue& operator=(const TaskQueue&) = delete;
+    TaskQueue& operator=(TaskQueue&&) = delete;
+
+    bool empty(){
+        std::lock_guard<std::mutex> lock(mutex_);
+        return queue_.empty();
+    }
+    int size(){
+        std::lock_guard<std::mutex> lock(mutex_);
+        return queue_.size();
+    }
+    void enqueue(Func& task){
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.emplace(task);
+    }
+    bool dequeue(Func& func) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if(!queue_.empty()){
+            func = std::move(queue_.front());
+            queue_.pop();
+            return true;
+        }
+        return false;
+    }
+
+private:
+    std::queue<Func> queue_;
+    std::mutex mutex_;
+};
+
+/**************************
+* @author   Yuan.
+* @date     2022/9/30
+* @brief    线程池实现
+***************************/
+class ThreadPool{
+    friend class Thread;
+private:
+    /**************************
+    * @author   Yuan.
+    * @date     2022/9/30
+    * @brief    类内对象，线程
+    ***************************/
+    class Thread{
+        friend class ThreadPool;
+    public:
+        Thread(ThreadPool* pool) : thread_id_(count_++), thread_pool_(pool){
+            thread_ = std::thread(std::ref(*this));
+        }
+        Thread(const Thread&) = delete;
+        Thread(Thread&&) = delete;
+        Thread& operator=(const Thread&) = delete;
+        Thread& operator=(Thread&&) = delete;
+        void shutdown(){ is_shutdown_ = true; }
+        int getId() const { return thread_id_; }
+        void operator()(){
+            while(!is_shutdown_){
+                // if(func_count_ > 1000){
+                //     std::cout << std::endl;
+                // }else{
+                //     std::cout << func_count_ << std::endl;
+                // }
+                if(!thread_pool_->task_queue_.empty()){
+                    // std::cout << "thread " << thread_id_ << " queue has element " << func_count_ << std::endl;
+                    std::function<void()> func;
+                    bool is_get = false;
+                    {
+                        std::unique_lock<std::mutex> loc(thread_pool_->lock_);
+                        // std::cout << "thread " << thread_id_ << " get lock " << func_count_ << std::endl;
+                        if(thread_pool_->task_queue_.empty()){
+                            // std::cout << "thread " << thread_id_ << " wait " << func_count_ << std::endl;
+                            thread_pool_->cv_.wait(loc);
+                            // std::cout << "thread " << thread_id_ << " wakeup " << func_count_ << std::endl;
+                        }
+                        // std::cout << "thread " << thread_id_ << " get task " << func_count_ << std::endl;
+                        is_get = thread_pool_->task_queue_.dequeue(func);
+                    }
+                    if(is_get){
+                        // std::cout << "thread " << thread_id_ << " doing func " << func_count_ << std::endl;
+                        func_count_++;
+                        func();
+                    }
+                }
+            }
+        }
+    private:
+        int thread_id_;
+        bool is_shutdown_ = false;
+        ThreadPool* thread_pool_;
+        std::thread thread_;
+        static int count_;
+        static int func_count_;
+    };
+
+public:
+    ThreadPool(size_t num = 6) : thread_size_(num){
+        createThread(num);
+    }
+    ThreadPool(const ThreadPool &) = delete;
+    ThreadPool(ThreadPool &&) = delete;
+    ThreadPool &operator=(const ThreadPool &) = delete;
+    ThreadPool &operator=(ThreadPool &&) = delete;
+    ~ThreadPool(){
+        this->shutdown();
+    }
+    template<typename T>
+    void test(T&& t);
+
+    // 向线程池提交任务
+    template<typename Func, typename... Args>
+    auto submit(Func&& f, Args&&... args) -> std::future<decltype(f(args...))>{
+        // 1. 包装一层 function，消去参数
+        // <decltype(f(args...))()> 表示返回值为函数 f 的返回值的一个函数，但没有参数
+        // 参数用 std::bind 将 args 绑定到 func 上，所以 func 里不需要指定参数
+        // test<Args...>();
+        std::function<decltype(f(args...))()> func = std::bind(std::forward<Func>(f), std::forward<Args>(args)...);
+
+        // 2. 用 packaged_task 包装函数，结果可以用 future 获得，使函数获得异步特性
+        // packaged_task 不可复制，使用指针进行传递
+        auto tast_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
+
+        // 3. 再包装一层 function，使接口统一，可以放入队列
+        std::function<void()> unified_func = [tast_ptr](){
+            (*tast_ptr)();
+        };
+
+        task_queue_.enqueue(unified_func);
+        cv_.notify_one();
+        return tast_ptr->get_future();
+    }
+    // 查看线程数
+    int size() const { return thread_size_; }
+    // 真正运行的线程数
+    int workingSize() const { return threads_.size(); }
+    // 设置线程数
+    void setSize(int size){
+        if(size < thread_size_){
+            shutdownThread(thread_size_ - size);
+        }else{
+            createThread(size - thread_size_);
+        }
+        thread_size_ = size;
+    }
+    // 关闭线程池
+    void shutdown(){
+        shutdownThread(threads_.size());
+        int count = 0;
+        cv_.notify_all();
+        for(auto& ptr : threads_){
+            if(ptr->thread_.joinable()){
+                ptr->thread_.join();
+                delete ptr;
+            }
+        }
+    }
+private:
+    void shutdownThread(int num){
+        std::lock_guard<std::mutex> lock(vec_lock_);
+        for(int i = 0; i < num; i++){
+            threads_[i]->shutdown();
+        }
+    }
+    void createThread(int num){
+        for(int i = 0; i < num; i++){
+            threads_.push_back(new Thread(this));
+        }
+    }
+    TaskQueue<std::function<void()>> task_queue_;
+    std::vector<Thread*> threads_;
+    std::condition_variable cv_;
+    std::mutex lock_;
+    std::mutex vec_lock_;
+    int thread_size_;
+};
+int ThreadPool::Thread::count_ = 0;
+int ThreadPool::Thread::func_count_ = 0;
+
+}
---
+++ thread_test.cpp
@@ -0,0 +1,34 @@
+#include "thread_pool.h"
+#include <iostream>
+#include <functional>
+
+using namespace std;
+using namespace thread_pool;
+
+int test(atomic<int>& temp){
+    temp++;
+    int count = 0;
+    for(int i = 0; i < 1000; i++){
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    ThreadPool tp(20);
+    atomic<int> temp(0);
+    int times = 10;
+    chrono::system_clock::time_point time_point_now = chrono::system_clock::now();
+    for(int i = 0; i < times; i++){
+        // this_thread::sleep_for(chrono::milliseconds(5));
+        tp.submit(test, std::ref(temp));
+    }
+    while(temp < times){
+    }
+    cout << "done" << endl;
+    chrono::system_clock::time_point time_point_after = chrono::system_clock::now();
+    chrono::system_clock::duration duration = time_point_after - time_point_now;
+    time_t abc = chrono::duration_cast<chrono::milliseconds>(duration).count();
+    cout << "time: " << abc << "ms" << endl;
+    return 0;
+}
